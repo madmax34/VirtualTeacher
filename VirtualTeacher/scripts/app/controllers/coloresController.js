60c6aace5f140337f3a37b5770fb632ce6ca0f2a
madmax34 <topp.maximiliano@gmail.com>
2014-12-23 01:33:28 -0300

    Agregado juego de colores

--- VirtualTeacher/scripts/app/controllers/coloresController.js
+++ VirtualTeacher/scripts/app/controllers/coloresController.js
@@ -1,7 +1,104 @@
 ﻿var coloresController = angular.module('coloresController', []);
 
 coloresController.controller('ColoresController', ['$scope', '$log', 'Images', function ($scope, $log, Images) {
-    $scope.getImgSrc = function (src) {
-        return Images.getImgSrc(src);
+    $scope.puntos = 0;
+    $scope.timer = '00:00.000s';
+    $scope.gameCanvas = $("#gameCanvas");
+    $scope.columnWidth = ($scope.gameCanvas.width() / 3);
+    $scope.columnHeight = ($scope.gameCanvas.height() / 3);
+    $scope.minSize = $scope.columnWidth > $scope.columnHeight ? $scope.columnHeight : $scope.columnWidth;
+    $scope.widthRect = $scope.minSize - 10;
+    $scope.heightRect = $scope.widthRect;
+
+    var svgns = "http://www.w3.org/2000/svg";
+
+    $scope.drawRect = function(x, y, height, width, id) {
+        var rect = document.createElementNS(svgns, 'rect');
+        rect.setAttributeNS(null, 'x', x);
+        rect.setAttributeNS(null, 'y', y);
+        rect.setAttributeNS(null, 'height', height);
+        rect.setAttributeNS(null, 'width', width);
+        rect.setAttributeNS(null, 'fill', colores_lv1[id].colour);
+        rect.setAttributeNS(null, 'data-nombre', colores_lv1[id].name);
+        rect.setAttributeNS(null, 'ng-click', 'elementClick()');
+        return rect;
     };
+
+    $scope.shuffle = function (array) {
+        var currentIndex = array.length, temporaryValue, randomIndex;
+
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+            array[randomIndex].id = randomIndex;
+        }
+
+        return array;
+    };
+
+    $scope.getRandomInPeriod = function (min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+
+    $scope.drawBoard = function () {
+        $scope.shuffledArray = $scope.shuffle(colores_lv1);
+        $scope.elementoBuscadoSvg = $("#elementoBuscado");
+        $scope.elementoBuscadoRect = $scope.drawRect(2, 2, 50, 50, $scope.getRandomInPeriod(0, 7));
+        $scope.elementoBuscadoSvg.append($scope.elementoBuscadoRect);
+        $scope.ArrayIndex = 0;
+
+        for (var i = 0; i < 3; i++) {
+            $scope.posY = 5 + $scope.minSize * i;
+
+            for (var j = 0; j < 3; j++) {
+                setTimeout(true, 500);
+                $scope.posX = 5 + $scope.minSize * j;
+                if ($scope.ArrayIndex < $scope.shuffledArray.length) {
+                    var rect = $scope.drawRect($scope.posX, $scope.posY, $scope.widthRect, $scope.heightRect, $scope.ArrayIndex);
+                    $scope.gameCanvas.append(rect);
+                } else {
+                    break;
+                }
+                $scope.ArrayIndex++;
+            }
+        }
+
+        $("rect").on("click", function () {
+
+            if ($scope.elementoBuscadoRect) {
+                var nombreBuscado = $scope.elementoBuscadoRect.getAttribute('data-nombre');
+                var nombre = $(this).attr('data-nombre');
+
+                if (nombreBuscado == nombre) {
+                    $scope.puntos += 50;
+                    $scope.$apply();
+                    $scope.drawBoard();
+                } else {
+                    $scope.puntos -= 25;
+                    $scope.$apply();
+                }
+            }
+        });
+    };
+
+    $scope.drawBoard();
+
+    
+
+
+//$scope.drawRect(70, 10, $scope.widthRect, $scope.heightRect, 1);
+    //$scope.drawRect(130, 10, $scope.widthRect, $scope.heightRect, 2);
+    //$scope.drawRect(10, 70, $scope.widthRect, $scope.heightRect, 3);
+    //$scope.drawRect(70, 70, $scope.widthRect, $scope.heightRect, 4);
+    //$scope.drawRect(130, 70, $scope.widthRect, $scope.heightRect, 5);
+    //$scope.drawRect(10, 130, $scope.widthRect, $scope.heightRect, 6);
+    //$scope.drawRect(70, 130, $scope.widthRect, $scope.heightRect, 7);
 }]);
---
+++ VirtualTeacher/scripts/app/models/colores.js
@@ -0,0 +1,57 @@
+﻿var colores_lv1 = [
+    {
+        id: 0,
+        name: 'rojo',
+        colour: '#FF0000',
+        tipo: 'primario'
+    },
+    {
+        id: 1,
+        name: 'amarillo',
+        colour: '#FFFF00',
+        tipo: 'primario'
+    },
+    {
+        id: 2,
+        name: 'azul',
+        colour: '#0000FF',
+        tipo: 'primario'
+    },
+    {
+        id: 3,
+        name: 'verde',
+        colour: '#00FF00',
+        tipo: 'secundario'
+    },
+    {
+        id: 4,
+        name: 'anaranjado',
+        colour: '#FFA500',
+        tipo: 'secundario'
+    },
+    {
+        id: 5,
+        name: 'violeta',
+        colour: '#FF00FF',
+        tipo: 'secundario'
+    },
+    {
+        id: 6,
+        name: 'negro',
+        colour: '#000000',
+        tipo: 'neutro'
+    },
+    {
+        id: 7,
+        name: 'blanco',
+        colour: '#FFFFFF',
+        tipo: 'neutro'
+    },
+    {
+        id: 8,
+        name: 'marron',
+        colour: '#BA7F03',
+        tipo: ''
+    }
+
+];
